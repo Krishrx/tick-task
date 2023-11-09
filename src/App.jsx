ee35e6392b5f04008cc116c21c766ee86de98d26
Krishrx <kalikrishnas342@gmail.com>
2023-11-10 01:14:36 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Notification from "./components/Notification"
 import TaskFields from "./components/TaskFields"
 import { useSelector } from "react-redux"
 import TodoTask from "./components/TodoTask";
+import Footer from "./components/Footer";
 function App() {
   const {showToast,typeOfToast,messageOnToast} = useSelector(state => state.toast);
   return (
@@ -10,7 +11,8 @@ function App() {
       <Header />
       <Notification showToast={showToast} typeOfToast={typeOfToast} messageOnToast={messageOnToast}/>
       <TaskFields />
-      <TodoTask/>
+      <TodoTask />
+      <Footer/>
       </div>
   )
 }
